3faea86924ef3da1b285ceb1dd79bd0248a7e40e
505e06b2 <505e06b2@gmail.com>
2023-03-18 23:47:58 +0000

    Utilise Async/Await more in buttons.mjs - _increaseRate no longer uses intervals - functions have been replaced with arrow functions - superfluous async functions have been made synchronous

--- js/buttons.mjs
+++ js/buttons.mjs
@@ -3,30 +3,27 @@ import Noise from "./noise.mjs";
 import MediaSessionHandler from "./media_session_handler.mjs";
 
 function _Buttons() {
-	let _speedup_interval = null;
+	let _speedup_in_progress = false;
 
-	const _increaseRate = (current_rate, end_rate) => {
+	const _increaseRate = async (current_rate, end_rate) => {
+		_speedup_in_progress = true;
+
 		const step = 0.01 * ((current_rate > end_rate) ? -1 : 1);
 
-		const cleanUp = () => {
-			CassetteAudio.rate.set(end_rate);
-			clearInterval(_speedup_interval);
-			_speedup_interval = null;
-		}
-
-		const rateChange = () => {
+		do {
 			CassetteAudio.rate.set(current_rate);
 
 			current_rate += step;
-			if(step > 0 && current_rate >= end_rate) cleanUp();
-			else if(step < 0 && current_rate <= end_rate) cleanUp();
-		}
+			await new Promise(x => setTimeout(x, 1));
 
-		rateChange(); //do it before resume at least once
-		_speedup_interval = setInterval(rateChange, 1);
+		} while(_speedup_in_progress && ((step > 0 && current_rate < end_rate) || (step < 0 && current_rate > end_rate)));
+
+
+		CassetteAudio.rate.set(end_rate);
+		_speedup_in_progress = false;
 	};
 
-	this.Play = async function() {
+	this.Play = async () => {
 		const button_classes = document.getElementById("button_play").classList;
 		if(button_classes.contains("pressed")) return;
 
@@ -38,38 +35,33 @@ function _Buttons() {
 		await CassetteAudio.resume();
 	}
 
-	this.Stop = async function() {
+	this.Stop = async () => {
 		await CassetteAudio.suspend();
 		Noise.pause();
 		cassette.style.animationPlayState = "paused";
 
-		if(_speedup_interval) {
-			clearInterval(_speedup_interval);
-			_speedup_interval = null;
-		}
+		if(_speedup_in_progress) _speedup_in_progress = false;
 
 		document.getElementById("button_play").classList.remove("pressed");
 		CassetteAudio.rate.set(1.0);
 	}
 
-	this.PlaySpeed = async function(amount) {
-		if(CassetteAudio.state() !== "running" || _speedup_interval) return;
+	this.PlaySpeed = (amount) => {
+		if(CassetteAudio.state() !== "running" || _speedup_in_progress) return;
 
-		let requested = (CassetteAudio.rate.get() + amount).toFixed(1);
-		if(requested < 0.5) requested = 0.5;
-		else if(requested > 1.5) requested = 1.5;
+		const requested = (CassetteAudio.rate.get() + amount).toFixed(1);
 
-		_increaseRate(CassetteAudio.rate.get(), requested);
+		_increaseRate(CassetteAudio.rate.get(), Math.min(Math.max(requested, 0.5), 1.5));
 		//cassette.style.animationDuration = `${default_animation_time/requested}s`; //Jutters really bad - not used anymore at all
 	}
 
-	this.Volume = async function(value) {
+	this.Volume = (value) => {
 		const vol = value / 100;
 		CassetteAudio.gain.set(vol);
 		Noise.volume.set(vol);
 	}
 
-	this.Eject = async function() {
+	this.Eject = async () => {
 		await this.Stop();
 		MediaSessionHandler.remove();
 		closeCassette();
